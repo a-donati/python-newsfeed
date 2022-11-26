a900098374a368e30fa4777d5cbc947976d4814f
Angela Donati <angeladonati93@gmail.com>
2022-11-26 17:32:27 -0500

    add user validation decorator

--- app/models/User.py
+++ app/models/User.py
@@ -2,6 +2,9 @@ from app.db import Base
 # map models to MySQL tables
 from sqlalchemy import Column, Integer, String
 # create User class that inherits Base class
+from sqlalchemy.orm import validates
+# validates function decorator SQLAlchemy
+
 # declare several properties that parent Base class will use
 # to make the table
 class User(Base):
@@ -10,3 +13,15 @@ class User(Base):
   username = Column(String(50), nullable=False)
   email = Column(String(50), nullable=False, unique=True)
   password = Column(String(100), nullable=False)
+# validate email
+  @validates('email')
+  def validate_email(self, key, email):
+    # make sure email address contains @ character
+    assert '@' in email
+    return email
+    # validate password
+  @validates('password')
+  def validate_password(self, key, password):
+    assert len(password) > 4
+
+    return password
