837b9571976ba9f1732464106403b672bc6905aa
Angela Donati <angeladonati93@gmail.com>
2022-11-26 17:15:55 -0500

    comments

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -8,5 +8,7 @@ load_dotenv()
 
 # connect to database using env variable
 engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
+# temp connections for CRUD operations
 Session = sessionmaker(bind=engine)
+# map models to MySQL tables
 Base = declarative_base()
