f95dd79fb5fd96cc23259b76cece834034f42f9b
Angela Donati <angeladonati93@gmail.com>
2022-11-26 17:28:02 -0500

    add seeds data

--- seeds.py
+++ seeds.py
@@ -5,3 +5,19 @@ from app.db import Session, Base, engine
 Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
 # creates any tables that Base mapped, in a class that inherits Base (like User)
+
+# establish temp session connection thru SQLAlchemy for CRUD operations
+db = Session()
+
+# insert array of users
+db.add_all([
+  User(username='alesmonde0', email='nwestnedge0@cbc.ca', password='password123'),
+  User(username='jwilloughway1', email='rmebes1@sogou.com', password='password123'),
+  User(username='iboddam2', email='cstoneman2@last.fm', password='password123'),
+  User(username='dstanmer3', email='ihellier3@goo.ne.jp', password='password123'),
+  User(username='djiri4', email='gmidgley4@weather.com', password='password123')
+])
+# INSERT
+db.commit()
+# close the session
+db.close()
