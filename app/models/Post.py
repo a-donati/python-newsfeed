9e778056a6240e699538a2eb0f5cef895a2ff247
Angela Donati <angeladonati93@gmail.com>
2022-11-27 16:41:58 -0500

    update Post model

---
+++ app/models/Post.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from app.db import Base
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+
+# write SQLAlchemy models as Python classes
+class Post(Base):
+  __tablename__ = 'posts'
+  id = Column(Integer, primary_key=True)
+  title = Column(String(100), nullable=False)
+  post_url = Column(String(100), nullable=False)
+#   foreign key
+  user_id = Column(Integer, ForeignKey('users.id'))
+#   use built in Python datetime module to generate timestamps
+  created_at = Column(DateTime, default=datetime.now)
+  updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
