90c1f2783e743755c3672f333671f1306a6a6716
Angela Donati <angeladonati93@gmail.com>
2023-01-28 17:29:42 -0500

    establish api setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from app.routes import home, dashboard
+from app.routes import home, dashboard, api
 # import our app routes from routes folder
 from app.db import init_db
 # import init_db func
@@ -11,7 +11,8 @@ def create_app(test_config=None):
     app = Flask(__name__, static_url_path="/")
     app.url_map.strict_slashes = False
     app.config.from_mapping(SECRET_KEY="super_secret_key")
-
+    # any routes defined in api.py module will automatically become part of flask app and have the prefix of /api
+    app.register_blueprint(api)
     @app.route("/hello")
     def hello():
         return "hello world"
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,3 +2,5 @@ from .home import bp as home
 # import home.py module from current directory, import bp object as home
 from .dashboard import bp as dashboard
 # importat dashboard.py module from current directoy import bp object as dashboard
+from .api import bp as api
+# important api blueprint
