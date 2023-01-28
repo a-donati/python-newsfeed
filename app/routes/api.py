45b09924498b3619cd5082f140e64d7d882d4d3d
Angela Donati <angeladonati93@gmail.com>
2023-01-28 17:41:54 -0500

    add error handling

--- app/routes/api.py
+++ app/routes/api.py
@@ -6,19 +6,23 @@ bp = Blueprint('api', __name__, url_prefix='/api')
 
 # route will resolve to - /api/users - POST route
 @bp.route('/users', methods=['POST'])
+
 def signup():
   data = request.get_json()
   db = get_db()
 
-  # create a new user
-  newUser = User(
-    username = data['username'],
-    email = data['email'],
-    password = data['password']
-  )
-
+  try:
+    # attempt creating a new user
+    newUser = User(
+      username = data['username'],
+      email = data['email'],
+      password = data['password']
+    )
   # save in database - prep INSERT statement
-  db.add(newUser)
-  db.commit()
+    db.add(newUser)
+    db.commit()
+  except:
+    # insert failed, so send error to front end
+    return jsonify(message = 'Signup failed'), 500
 
   return jsonify(id = newUser.id)
