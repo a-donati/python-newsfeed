fe5333fe9cc3e2bfe0bc31a526781eef3b06a995
Angela Donati <angeladonati93@gmail.com>
2023-01-28 17:34:24 -0500

    update login route

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,5 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from app.models import User
 from app.db import get_db
-
+# establish api blueprint
 bp = Blueprint('api', __name__, url_prefix='/api')
+
+# route will resolve to - /api/users - POST route
+@bp.route('/users', methods=['POST'])
+def signup():
+  data = request.get_json()
+  print(data)
+
+  return ''
