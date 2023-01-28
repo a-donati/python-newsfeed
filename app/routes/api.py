9704eaa8d0861f138191bb8e1c7eb3c55a4e6dab
Angela Donati <angeladonati93@gmail.com>
2023-01-28 17:44:04 -0500

    import sys

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from app.models import User
 from app.db import get_db
+import sys
 # establish api blueprint
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -23,6 +24,8 @@ def signup():
     db.commit()
   except:
     # insert failed, so send error to front end
+    print(sys.exe_info()[0])
     return jsonify(message = 'Signup failed'), 500
-
+    
+# An AssertionError is thrown when our custom validations fail. An IntegrityError is thrown when something specific to MySQL (like a UNIQUE constraint) fails.
   return jsonify(id = newUser.id)
