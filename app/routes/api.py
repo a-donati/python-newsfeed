4be96dbe4c8eff4d9c4c7a45fa60eb9324af2de9
Angela Donati <angeladonati93@gmail.com>
2023-04-09 13:46:43 -0400

    add CRUD functionality for posts

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, request, jsonify, session
 from app.models import User, Post, Comment, Vote
 from app.db import get_db
+from app.utils.auth import login_required
 import sys
+
 # establish api blueprint
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -66,6 +68,7 @@ def login():
     return jsonify(id=user.id)
 
 @bp.route('/comments', methods=['POST'])
+@login_required
 def comment():
   data = request.get_json()
   db = get_db()
@@ -88,6 +91,7 @@ def comment():
   return jsonify(id = newComment.id)
 
 @bp.route('/posts/upvote', methods=['PUT'])
+@login_required
 def upvote():
   data = request.get_json()
   db = get_db()
@@ -110,6 +114,7 @@ def upvote():
   return '', 204
 
 @bp.route('/posts', methods=['POST'])
+@login_required
 def create():
   data = request.get_json()
   db = get_db()
@@ -133,6 +138,7 @@ def create():
   return jsonify(id = newPost.id)
 
 @bp.route('/posts/<id>', methods=['PUT'])
+@login_required
 def update(id):
   data = request.get_json()
   db = get_db()
